6e982c73f215f4ca08de9e5ab27e317290f59ef0
ihollander <ianholla@gmail.com>
2020-03-14 12:46:27 -0400

    solution

--- index.js
+++ index.js
@@ -19,6 +19,13 @@ function renderPlayer(player) {
     <button class="like-button">❤️</button>
   `
 
+  // Nested Event Listener for likes
+  // playerDiv.querySelector(".like-button").addEventListener("click", e => {
+  //   player.likes++
+  //   const likesP = playerDiv.querySelector(".likes")
+  //   likesP.textContent = `${player.likes} likes`
+  // })
+
   // append the element to the container
   playerContainer.append(playerDiv)
 }
@@ -40,7 +47,32 @@ function toggleColor(element) {
   }
 }
 
+document.querySelector("#header").addEventListener("click", e => {
+  toggleColor(e.target)
+})
 
 /***** Deliverable 2 *****/
+document.querySelector("#new-player-form").addEventListener("submit", e => {
+  e.preventDefault()
+
+  const newPlayer = {
+    number: e.target.number.value,
+    name: e.target.name.value,
+    nickname: e.target.nickname.value,
+    photo: e.target.photo.value,
+    likes: 0
+  }
+
+  renderPlayer(newPlayer)
+})
 
 /***** Deliverable 3 *****/
+// Event Delegation (see above for nested event listener approach)
+document.querySelector(".player-container").addEventListener("click", e => {
+  if (e.target.matches(".like-button")) {
+    const playerDiv = e.target.closest(".player")
+    const likesP = playerDiv.querySelector(".likes")
+    const newLikes = parseInt(likesP.textContent) + 1
+    likesP.textContent = `${newLikes} likes`
+  }
+})
